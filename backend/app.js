4baf001631b2dbeb1cf858a0caf5c22e9fd5d363
Alvaro Gonzalez <alvgonfri@alum.us.es>
2024-03-22 02:27:25 +0100

    chore: Set deployment configuration

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
-app.use(cors({ credentials: true, origin: process.env.CORS_ORIGIN_LOCAL }));
+app.use(cors({ credentials: true, origin: process.env.CORS_ORIGIN }));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
