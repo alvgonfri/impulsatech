ad3122b680151a7dd01f8f6e1ed0733b6a1b7550
Alvaro Gonzalez <alvgonfri@alum.us.es>
2024-03-23 14:21:44 +0100

    ci: Show test response body via console

--- backend/test/api/auth.test.js
+++ backend/test/api/auth.test.js
@@ -20,7 +20,7 @@ describe("Auth tests", () => {
                 email: "test@gmail.com",
                 password: "12345678",
             });
-            console.log("Test response: ", response);
+            console.log("Test response: ", response.body);
             expect(response.statusCode).toBe(201);
         });
 
@@ -41,7 +41,7 @@ describe("Auth tests", () => {
                 email: "test@gmail.com",
                 password: "12345678",
             });
-            console.log("Test response: ", response);
+            console.log("Test response: ", response.body);
             expect(response.statusCode).toBe(200);
         });
 
