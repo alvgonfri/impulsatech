d750390fbac8cd7a2946d1fa007d57e42249e09e
Alvaro Gonzalez <alvgonfri@alum.us.es>
2024-03-30 20:54:35 +0100

    fix: Set cookie max age

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -29,6 +29,7 @@ export const register = async (req, res) => {
                 httpOnly: true,
                 secure: true,
                 sameSite: "none",
+                maxAge: 24 * 60 * 60 * 1000,
             });
         } else res.cookie("token", token);
 
@@ -73,6 +74,7 @@ export const login = async (req, res) => {
                 httpOnly: true,
                 secure: true,
                 sameSite: "none",
+                maxAge: 24 * 60 * 60 * 1000,
             });
         } else res.cookie("token", token);
 
