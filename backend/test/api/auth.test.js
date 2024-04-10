88df2c989d2e7650087be157bf707907f08fd518
Alvaro Gonzalez <alvgonfri@alum.us.es>
2024-04-10 18:41:04 +0200

    test: Modify auth tests order

--- backend/test/api/auth.test.js
+++ backend/test/api/auth.test.js
@@ -34,6 +34,24 @@ describe("Auth tests", () => {
         });
     });
 
+    describe("POST /api/v1/login (user)", () => {
+        it("should return 200 OK", async () => {
+            const response = await request(app).post("/api/v1/login").send({
+                email: "test@gmail.com",
+                password: "12345678",
+            });
+            expect(response.statusCode).toBe(200);
+        });
+
+        it("should return 400 Bad Request", async () => {
+            const response = await request(app).post("/api/v1/login").send({
+                email: "test@gmail.com",
+                password: "wrongpassword",
+            });
+            expect(response.statusCode).toBe(400);
+        });
+    });
+
     describe("POST /api/v1/register-organization", () => {
         it("should return 201 Created", async () => {
             const response = await request(app)
@@ -58,29 +76,13 @@ describe("Auth tests", () => {
         });
     });
 
-    describe("POST /api/v1/login", () => {
-        it("should return 200 OK", async () => {
-            const response = await request(app).post("/api/v1/login").send({
-                email: "test@gmail.com",
-                password: "12345678",
-            });
-            expect(response.statusCode).toBe(200);
-        });
-
+    describe("POST /api/v1/login (organization)", () => {
         it("should return 200 OK", async () => {
             const response = await request(app).post("/api/v1/login").send({
                 email: "test_org@gmail.com",
                 password: "12345678",
             });
             expect(response.statusCode).toBe(200);
-        });
-
-        it("should return 400 Bad Request", async () => {
-            const response = await request(app).post("/api/v1/login").send({
-                email: "test@gmail.com",
-                password: "wrongpassword",
-            });
-            expect(response.statusCode).toBe(400);
         });
 
         it("should return 400 Bad Request", async () => {
