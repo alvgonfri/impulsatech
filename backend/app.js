7c5936655643dbdca0fc9cb61ae12c9fa0442f24
Alvaro Gonzalez <alvgonfri@alum.us.es>
2024-04-22 16:39:49 +0200

    fix: Update cors origin

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const app = express();
 
 const corsOptions = {
     credentials: true,
-    origin: process.env.CORS_ORIGIN,
+    // origin: process.env.CORS_ORIGIN,
 };
 
 app.use(cors(corsOptions));
