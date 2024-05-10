a652aefcfb10d5f7fd46a1abe0539be7b1ed1913
Alvaro Gonzalez <alvgonfri@alum.us.es>
2024-05-10 02:08:30 +0200

    feat: Add cron job to update campaign status

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,16 @@
+import cron from "node-cron";
 import app from "./app.js";
 import { connectDB } from "./db.js";
+import { updateCampaigns } from "./libs/updateCampaigns.js";
 
 connectDB();
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running on port", process.env.PORT || 3000);
+});
+
+// Cron job to check campaigns every hour
+cron.schedule("0 * * * *", () => {
+    console.log("Running cron job to check campaigns");
+    updateCampaigns();
 });
